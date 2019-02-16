e8f621fefa9f9b7ca159d1b6eae859039015e9c0
mt8 <info@mt8.biz>
2019-02-16 20:55:12 +0900

    Use home_url () instead of $ _ SERVER to get the host name in get_current_page_url()

--- src/functions.php
+++ src/functions.php
@@ -107,21 +107,7 @@ if ( ! function_exists( 'untrailingslashit' ) ) {
 if ( ! function_exists( 'get_current_page_url' ) ) {
 
   function get_current_page_url() {
-    $pageURL = 'http';
-
-    if ( array_key_exists( 'HTTPS', $_SERVER ) && $_SERVER["HTTPS"] == "on" ) {
-      $pageURL = "https";
-    }
-
-    $pageURL .= "://";
-
-    if ( $_SERVER["SERVER_PORT"] != "80" ) {
-      $pageURL .= $_SERVER["SERVER_NAME"] . ":" . $_SERVER["SERVER_PORT"] . $_SERVER["REQUEST_URI"];
-    } else {
-      $pageURL .= $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"];
-    }
-
-    return $pageURL;
+    return home_url() . $_SERVER["REQUEST_URI"];
   }
 }
 
